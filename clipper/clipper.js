aa8a8d1bbc2425c2f4ddc4dc08f3ff22716ad11e
quenousimporte <quenousimporte@riseup.net>
2024-01-08 16:24:34 +0100

    added: date in clipper

--- clipper/clipper.js
+++ clipper/clipper.js
@@ -3,7 +3,8 @@ javascript: (function()
 	var notesurl = "";
 	var bm = {
 		title: document.title,
-		url: document.location.href
+		url: document.location.href,
+		time: Date.now()
 	};
 	window.open("https://" + notesurl + "?c=" + encodeURIComponent(JSON.stringify(bm)), "_blank", "popup,width=100,height=100");
 })();
